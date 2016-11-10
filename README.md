fbdeea2a877c87327b7e98f8d1f859acbe1a8c34
amerigroup <andywj2ee@gmail.com>
2016-11-10 23:01:22 +0800

    add baidu map in the page

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-1 install nodejs
-2 cd the tradeshift path
-3
+FindMovie project
+
+Prerequisites
+
+You will need the following things properly installed on your computer.
+
+Node.js (with NPM) and Bower
+Building
+
+Preparing
  tradshift>npm install bower -g
   tradshift>  bower install angular angular-route angular-resource
    tradshift>  bower install
-4. open index.html
+
+default
+gulp
+
+develop with realtime monitor, automatic open browser to view example
+build
+gulp build
+
+Build file to dist
+publish
+gulp publish
+
+Build & bump npm versions
+
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute','echarts-ng'])
+angular.module('myApp.view1', ['ngRoute','angular-echarts'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -55,28 +55,52 @@ angular.module('myApp.view1', ['ngRoute','echarts-ng'])
   $scope.remove = function(index) {
     $scope.items.splice(index, 1);
   };
-    var myChart = echarts.init(document.getElementById('echarts'));
-    $scope.DISTRIBUTION_ID = $echarts.generateInstanceIdentity();
-    $scope.distribution = {
-        xAxis : [
-            {
-                type : 'category',
-                data : ['周一','周二','周三','周四','周五','周六','周日']
-            }
-        ],
-        yAxis : [
-            {
-                type : 'value'
-            }
-        ],
-        series : [
-            {
-                name:'联盟广告',
-                type:'bar',
-                data:[220, 182, 191, 234, 290, 330, 310]
-            }
+/*
+    var pageload = {
+        name: 'page.load',
+        datapoints: [
+            { x: 2001, y: 1012 },
+            { x: 2002, y: 1023 },
+            { x: 2003, y: 1045 },
+            { x: 2004, y: 1062 },
+            { x: 2005, y: 1032 },
+            { x: 2006, y: 1040 },
+            { x: 2007, y: 1023 },
+            { x: 2008, y: 1090 },
+            { x: 2009, y: 1012 },
+            { x: 2010, y: 1012 },
         ]
     };
+
+    var firstPaint = {
+        name: 'page.firstPaint',
+        datapoints: [
+            { x: 2001, y: 22 },
+            { x: 2002, y: 13 },
+            { x: 2003, y: 35 },
+            { x: 2004, y: 52 },
+            { x: 2005, y: 32 },
+            { x: 2006, y: 40 },
+            { x: 2007, y: 63 },
+            { x: 2008, y: 80 },
+            { x: 2009, y: 20 },
+            { x: 2010, y: 25 },
+        ]
+    };
+
+    $scope.config = {
+        title: 'Line Chart',
+        subtitle: 'Line Chart Subtitle',
+        debug: true,
+        showXAxis: true,
+        showYAxis: true,
+        showLegend: true,
+        stack: false,
+    };
+
+    $scope.data = [ pageload ];
+    $scope.multiple = [pageload, firstPaint ];*/
+
 }])
 
 
