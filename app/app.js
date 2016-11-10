1ecaab93c132c3502085d242330ca5c956d7c6b2
amerigroup <andywj2ee@gmail.com>
2016-11-10 16:27:16 +0800

    my first version for tradeshift

--- app/app.js
+++ app/app.js
@@ -5,8 +5,8 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.common',
-  'myApp.version'
+  'myApp.common'
+ /* 'myApp.version'*/
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute','angularUtils.directives.dirPagination'])
+angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
